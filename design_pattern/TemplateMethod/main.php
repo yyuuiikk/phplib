ceef77a58e8ea673092df84bedef1e496d88f814
yyuuiikk <yutaikawa0315@gmail.com>
2019-06-01 16:15:21 +0900

    StringDisplayを作成

--- design_pattern/TemplateMethod/main.php
+++ design_pattern/TemplateMethod/main.php
@@ -1,6 +1,13 @@
 <?php
 
 require_once 'CharDisplay.php';
+require_once 'StringDisplay.php';
 
 $display = new \TemplateMethod\CharDisplay('Hello!!');
 $display->print();
+
+
+$st_disp = new \TemplateMethod\StringDisplay('goodbye');
+$st_disp->open();
+$st_disp->print();
+$st_disp->close();
